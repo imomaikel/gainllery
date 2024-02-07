fafc100074f456302eb7dcd71e2486a1cc1fc172
imomaikel <imomaikeldev@gmail.com>
2024-02-07 18:59:44 +0100

    fix: preview centering while zooming

--- src/renderer/src/routes/View.tsx
+++ src/renderer/src/routes/View.tsx
@@ -1,57 +1,90 @@
 import { ReactZoomPanPinchRef, TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
+import { ValueAnimationTransition, useAnimate } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
 import { Button } from '@/components/ui/button';
 
 const View = () => {
   const fetchedImages = window.store.get('fetchedFiles') as string[];
-  const transformComponentRef = useRef<ReactZoomPanPinchRef | null>(null);
-
+  const transformComponentRef = useRef<ReactZoomPanPinchRef>(null);
   const [index, setIndex] = useState(0);
+  const [scope, animate] = useAnimate();
 
+  // State
+  const isNext = index + 1 !== fetchedImages.length;
+  const isPrevious = index !== 0;
+
+  // Switch files
+  // TODO Settings
+  const animation: ValueAnimationTransition = { type: 'keyframes', duration: 0.15 };
+  const nextFile = () => {
+    if (!isNext) return;
+    setIndex(index + 1);
+    animate(scope.current, { x: ['100%', '0%'] }, { ...animation });
+  };
+  const previousFile = () => {
+    if (!isPrevious) return;
+    setIndex(index - 1);
+    animate(scope.current, { x: ['-100%', '0%'] }, { ...animation });
+  };
+
+  // Auto resize
   useEffect(() => {
     const onResize = () => {
-      transformComponentRef.current?.resetTransform();
+      // TODO Settings
+      transformComponentRef.current?.centerView(1, 0);
     };
 
     window.addEventListener('resize', onResize);
     return () => window.removeEventListener('resize', onResize);
   }, []);
 
+  const onLoad = () => transformComponentRef.current?.centerView(1, 0);
+
+  // Key binds
+  useEffect(() => {
+    const handleKeypress = (event: KeyboardEvent) => {
+      if (event.key === 'ArrowRight') {
+        nextFile();
+      } else if (event.key === 'ArrowLeft') {
+        previousFile();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeypress);
+    return () => window.removeEventListener('keydown', handleKeypress);
+  }, [nextFile, previousFile]);
+
   // TODO
   const url = `atom://${fetchedImages[index].replace(/ /gi, '%20').replace(/\?/gi, '%3F')}`;
 
   return (
-    <div className="h-full w-full">
-      <div className="flex h-full w-full flex-wrap items-center justify-center">
-        <TransformWrapper ref={transformComponentRef} maxScale={10}>
-          <TransformComponent>
+    <div className="relative flex h-screen w-screen items-center">
+      <TransformWrapper
+        disablePadding
+        wheel={{ step: 0.2, smoothStep: 0.003 }}
+        ref={transformComponentRef}
+        centerOnInit
+      >
+        <TransformComponent wrapperClass="!w-screen !h-screen">
+          <div ref={scope}>
             {url.endsWith('mp4') || url.endsWith('mp3') ? (
-              <video src={url} controls autoPlay className="h-screen object-contain object-center" draggable={false} />
+              <video
+                src={url}
+                controls
+                className="max-h-screen object-contain"
+                draggable={false}
+                onLoadedData={onLoad}
+              />
             ) : (
-              <img src={url} alt="test" draggable={false} className="h-screen object-contain object-center" />
+              <img src={url} className="max-h-screen object-contain" onLoad={onLoad} />
             )}
-          </TransformComponent>
-        </TransformWrapper>
+          </div>
+        </TransformComponent>
+      </TransformWrapper>
 
-        <div className="fixed top-0 space-x-4">
-          {/* TODO */}
-          <Button
-            onClick={() => {
-              setIndex(index - 1);
-              transformComponentRef.current?.resetTransform();
-            }}
-          >
-            PREV
-          </Button>
-          <Button
-            onClick={() => {
-              setIndex(index + 1);
-              transformComponentRef.current?.resetTransform();
-            }}
-          >
-            NEXT
-          </Button>
-        </div>
+      <div className="fixed top-0 flex w-full justify-center space-x-2">
+        <Button onClick={previousFile}>PREV</Button>
+        <Button onClick={nextFile}>NEXT</Button>
       </div>
     </div>
   );
