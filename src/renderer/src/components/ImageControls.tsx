a10b4ef949cba35841be621b1cfdfce81c734fba
imomaikel <imomaikeldev@gmail.com>
2024-04-11 21:18:24 +0200

    feat: add the same design for both video and image controls

--- src/renderer/src/components/ImageControls.tsx
+++ src/renderer/src/components/ImageControls.tsx
@@ -1,18 +1,6 @@
-import {
-  AllSidesIcon,
-  ChevronDownIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  HomeIcon,
-  ZoomInIcon,
-  ZoomOutIcon,
-} from '@radix-ui/react-icons';
-import { FaRotateLeft, FaRotateRight } from 'react-icons/fa6';
 import { ReactZoomPanPinchRef } from 'react-zoom-pan-pinch';
 import React, { RefObject, useState } from 'react';
-import { Link } from 'react-router-dom';
-import ControlsBox from './ControlsBox';
-import { cn } from '@/lib/utils';
+import FileControls from './FileControls';
 
 type TImageControls = {
   isPrevious: boolean;
@@ -32,50 +20,21 @@ const ImageControls = React.forwardRef<ReactZoomPanPinchRef, TImageControls>(
     const center = () => ref.current?.centerView(1, 250);
 
     return (
-      <ControlsBox visible={menuVisible} onShow={() => setMenuVisible(true)}>
-        <div className="relative rounded-tl-lg rounded-tr-lg bg-white/10 px-6 py-2">
-          <div className="flex h-full w-full items-center space-x-4">
-            <div className="flex items-center space-x-2">
-              <div role="button" onClick={onPrevious} className={cn(!isPrevious && 'invisible')}>
-                <ChevronLeftIcon className="h-8 w-8 rounded-full bg-primary" />
-              </div>
-              <div role="button" onClick={onNext} className={cn(!isNext && 'invisible')}>
-                <ChevronRightIcon className="h-8 w-8 rounded-full bg-primary" />
-              </div>
-            </div>
-            <div className="flex items-center space-x-2">
-              <div role="button" onClick={zoomIn}>
-                <ZoomInIcon className="h-8 w-8 rounded-full bg-primary" />
-              </div>
-              <div role="button" onClick={zoomOut}>
-                <ZoomOutIcon className="h-8 w-8 rounded-full bg-primary" />
-              </div>
-            </div>
-            <div className="flex items-center space-x-2">
-              <div role="button" onClick={() => rotateMedia('LEFT')}>
-                <FaRotateLeft className="h-8 w-8 rounded-full bg-primary" />
-              </div>
-              <div role="button" onClick={() => rotateMedia('RIGHT')}>
-                <FaRotateRight className="h-8 w-8 rounded-full bg-primary" />
-              </div>
-            </div>
-            <div className="flex items-center space-x-2">
-              <div role="button" onClick={center}>
-                <AllSidesIcon className="h-8 w-8 rounded-full bg-primary" />
-              </div>
-              <div role="button" onClick={zoomOut}>
-                <Link to="/">
-                  <HomeIcon className="h-8 w-8 rounded-full bg-primary" />
-                </Link>
-              </div>
-              <div role="button" onClick={() => setMenuVisible(!menuVisible)}>
-                <ChevronDownIcon className="h-8 w-8 rounded-full bg-primary" />
-              </div>
-            </div>
-          </div>
-          <div className="absolute -z-10 h-full w-full backdrop-blur-sm" />
-        </div>
-      </ControlsBox>
+      <FileControls
+        isNext={isNext}
+        isPrev={isPrevious}
+        nextSlide={onNext}
+        onRotate={(side) => rotateMedia(side)}
+        prevSlide={onPrevious}
+        hideMenu={() => setMenuVisible(false)}
+        isMenuVisible={menuVisible}
+        props={{
+          fileType: 'image',
+          onCenter: center,
+          zoomIn,
+          zoomOut,
+        }}
+      />
     );
   },
 );
--- src/renderer/src/components/VideoControls.tsx
+++ src/renderer/src/components/VideoControls.tsx
@@ -1,17 +1,7 @@
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import React, { ElementRef, RefObject, useEffect, useState } from 'react';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa6';
-import { FaRotateRight, FaRotateLeft } from 'react-icons/fa6';
-import { FaPlay, FaPause } from 'react-icons/fa6';
 import { useSettings } from '@/hooks/settings';
-import { AiFillSound } from 'react-icons/ai';
-import { IoMdMore } from 'react-icons/io';
-import { Checkbox } from './ui/checkbox';
-import ControlsBox from './ControlsBox';
-import { Slider } from './ui/slider';
+import FileControls from './FileControls';
 import { getHMS } from '@/lib/utils';
-import { Button } from './ui/button';
-import { Label } from './ui/label';
 
 type TVideoControls = {
   isPrevious: boolean;
@@ -30,6 +20,7 @@ const VideoControls = React.forwardRef<ElementRef<'video'>, TVideoControls>(
     const [autoPlay, setAutoPlay] = useState(settings.get('autoPlay', false));
     const [totalTextDuration, setTotalTextDuration] = useState('00:00');
     const [currentSecond, setCurrentSecond] = useState(0);
+    const [menuVisible, setMenuVisible] = useState(true);
     const [autoRepeat, setAutoRepeat] = useState(false);
     const [mounted, setIsMounted] = useState(false);
     const [duration, setDuration] = useState(0);
@@ -123,82 +114,30 @@ const VideoControls = React.forwardRef<ElementRef<'video'>, TVideoControls>(
     if (!mounted || !ref.current) return null;
 
     return (
-      <ControlsBox onShow={() => {}} visible={true} className="bottom-1 w-screen select-none px-2">
-        <div className="flex space-x-2">
-          <div className="flex space-x-1">
-            <Button size="icon" onClick={onPrevious} disabled={!isPrevious}>
-              <FaChevronLeft />
-            </Button>
-            <Button onClick={playPause} size="icon">
-              {pause ? <FaPlay /> : <FaPause />}
-            </Button>
-            <Button size="icon" onClick={onNext} disabled={!isNext}>
-              <FaChevronRight />
-            </Button>
-          </div>
-          <div className="flex flex-1 items-center space-x-2">
-            <div>{getHMS(Math.round(currentSecond), duration).join(':')}</div>
-            <Slider
-              max={duration}
-              step={0.1}
-              defaultValue={[0]}
-              onValueChange={(e) => onSlide(e[0])}
-              value={[currentSecond]}
-            />
-            <div>{totalTextDuration}</div>
-          </div>
-          <div className="flex space-x-1">
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button size="icon">
-                  <IoMdMore className="h-6 w-6" />
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="relative w-min" align="center">
-                <div className="space-y-4">
-                  <div className="flex items-center space-x-2">
-                    <Label htmlFor="repeat">Repeat</Label>
-                    <Checkbox id="repeat" onCheckedChange={() => onRepeat()} checked={autoRepeat} />
-                  </div>
-                  <div className="flex items-center space-x-2">
-                    <Label htmlFor="repeat">Auto play</Label>
-                    <Checkbox id="repeat" onCheckedChange={() => onAutoPlay()} checked={autoPlay} />
-                  </div>
-                  <div className="flex items-center space-x-2">
-                    <Label>Rotate</Label>
-                    <Button size="smallIcon" onClick={() => rotateMedia('LEFT')}>
-                      <FaRotateLeft />
-                    </Button>
-                    <Button size="smallIcon" onClick={() => rotateMedia('RIGHT')}>
-                      <FaRotateRight />
-                    </Button>
-                  </div>
-                </div>
-              </PopoverContent>
-            </Popover>
-
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button size="icon">
-                  <AiFillSound className="h-4 w-4" />
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="relative h-[50vh] w-min">
-                <Slider
-                  orientation="vertical"
-                  value={[volume]}
-                  step={0.01}
-                  max={1}
-                  onValueChange={(e) => onVolume(e[0])}
-                />
-                <div className="absolute -top-5 left-0 flex w-full items-center text-sm text-muted-foreground">
-                  {Math.round(volume * 100)}%
-                </div>
-              </PopoverContent>
-            </Popover>
-          </div>
-        </div>
-      </ControlsBox>
+      <FileControls
+        isNext={isNext}
+        isPrev={isPrevious}
+        onRotate={(side) => rotateMedia(side)}
+        nextSlide={onNext}
+        prevSlide={onPrevious}
+        hideMenu={() => setMenuVisible(false)}
+        isMenuVisible={menuVisible}
+        props={{
+          fileType: 'video',
+          onAutoPlay: onAutoPlay,
+          onRepeat: onRepeat,
+          onVolume: (newVolume) => onVolume(newVolume),
+          playPause: playPause,
+          volume,
+          pause,
+          autoPlay,
+          autoRepeat,
+          currentSecond,
+          duration,
+          totalTextDuration,
+          onSeek: (timestamp) => onSlide(timestamp),
+        }}
+      />
     );
   },
 );
