2a4da8d7f2a96dfc77c694052af304f7a699b788
imomaikel <imomaikeldev@gmail.com>
2024-06-25 22:32:08 +0200

    feat: add file zoom pan pinch

--- src/renderer/src/routes/view/View.tsx
+++ src/renderer/src/routes/view/View.tsx
@@ -1,9 +1,31 @@
+import { ReactZoomPanPinchRef, TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
 import { useFileContext } from '@/hooks/useFileContext';
+import { useEventListener } from 'usehooks-ts';
+import { useRef } from 'react';
 
 const View = () => {
-  const { selectedFile } = useFileContext();
+  const { selectedFile, nextFile, isPrevious, isNext, previousFile } = useFileContext();
+  const transformComponentRef = useRef<ReactZoomPanPinchRef>(null);
 
-  return <div></div>;
+  useEventListener('resize', () => transformComponentRef.current?.centerView(1, 0));
+
+  return (
+    <div>
+      <TransformWrapper
+        ref={transformComponentRef}
+        key={selectedFile}
+        alignmentAnimation={{ sizeX: 0, sizeY: 0 }}
+        wheel={{ step: 0.2, smoothStep: 0.003 }}
+        centerOnInit
+        maxScale={20}
+        disablePadding
+      >
+        <TransformComponent wrapperClass="!w-screen !h-screen">
+          <img src={`atom://${selectedFile}`} className="max-h-screen" />
+        </TransformComponent>
+      </TransformWrapper>
+    </div>
+  );
 };
 
 export default View;
