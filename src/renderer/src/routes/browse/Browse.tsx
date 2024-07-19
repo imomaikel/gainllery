dcf649d29f20a9722a912c0590455a67ce089444
imomaikel <imomaikeldev@gmail.com>
2024-07-19 21:19:15 +0200

    feat(browse): add tile size slider

--- src/renderer/src/routes/browse/Browse.tsx
+++ src/renderer/src/routes/browse/Browse.tsx
@@ -1,11 +1,13 @@
 import { VirtuosoGrid, GridComponents } from 'react-virtuoso';
 import { forwardRef, useMemo, useState } from 'react';
+import { Slider } from '@/components/ui/slider';
+import { Label } from '@/components/ui/label';
 import { isFileVideo } from '@/lib/utils';
 import Item from './Item';
 
 const Browse = () => {
   const [files, setFiles] = useState<string[]>(window.store.get('favoriteFiles', []));
-  const [size, setSize] = useState(256);
+  const [tileSize, setTileSize] = useState(256);
 
   const filesWithData = useMemo(() => {
     const withData = files.map((filePath) => ({
@@ -19,22 +21,23 @@ const Browse = () => {
     () =>
       ({
         List: forwardRef(({ style, children }, ref) => (
-          <div ref={ref} style={{ ...style }} className="my-4 flex flex-wrap items-center justify-center gap-4">
+          <div ref={ref} style={{ ...style }} className="mt-4 flex flex-wrap items-center justify-center gap-4">
             {children}
           </div>
         )),
         Item: ({ children }) => (
           <div
             style={{
-              width: `${size}px`,
-              height: `${size}px`,
+              width: `${tileSize}px`,
+              height: `${tileSize}px`,
             }}
           >
             {children}
           </div>
         ),
+        Footer: () => <div className="pb-14" />,
       }) satisfies GridComponents,
-    [size],
+    [tileSize],
   );
 
   const handleRemoveFromFav = (path: string) => {
@@ -49,8 +52,17 @@ const Browse = () => {
         className="h-screen w-screen"
         totalCount={files.length}
         components={gridComponents}
-        itemContent={(idx) => <Item data={filesWithData[idx]} removeFromFavorites={handleRemoveFromFav} />}
+        itemContent={(idx) => (
+          <Item data={filesWithData[idx]} removeFromFavorites={handleRemoveFromFav} tileSize={tileSize} />
+        )}
       />
+      <div className="fixed bottom-0 flex h-10 w-screen items-center border-t bg-background/50 px-4 backdrop-blur-sm">
+        <div className="flex w-72 space-x-2">
+          <Label className="shrink-0">Tile Size</Label>
+          <Slider min={32} step={16} max={512} className="w-full" onValueChange={(value) => setTileSize(value[0])} />
+          <span className="text-xs">({tileSize})</span>
+        </div>
+      </div>
     </>
   );
 };
--- src/renderer/src/routes/browse/Item.tsx
+++ src/renderer/src/routes/browse/Item.tsx
@@ -1,4 +1,5 @@
 import { FaHeartBroken } from 'react-icons/fa';
+import { cn } from '@/lib/utils';
 
 type TItem = {
   data: {
@@ -6,24 +7,28 @@ type TItem = {
     isVideo: boolean;
   };
   removeFromFavorites: (path: string) => void;
+  tileSize: number;
 };
-const Item = ({ data, removeFromFavorites }: TItem) => {
+const Item = ({ data, removeFromFavorites, tileSize }: TItem) => {
   const { filePath, isVideo } = data;
 
   return (
     <div className="relative h-full w-full rounded border">
       {isVideo ? (
-        <video src={`atom://${filePath}`} className="h-full w-full rounded object-cover" />
+        <video src={`atom://${filePath}`} className="h-full w-full rounded object-cover" muted />
       ) : (
         <img src={`atom://${filePath}`} className="h-full w-full rounded object-cover" />
       )}
       <div
-        className="absolute right-2 top-2 cursor-pointer text-destructive opacity-50 hover:opacity-100"
+        className={cn(
+          'absolute right-2 top-2 cursor-pointer text-destructive opacity-50 hover:opacity-100',
+          tileSize <= 128 && 'right-1 top-1',
+        )}
         role="button"
         aria-label="remove from favorites"
         onDoubleClick={() => removeFromFavorites(filePath)}
       >
-        <FaHeartBroken className="h-6 w-6" />
+        <FaHeartBroken className={cn('h-6 w-6', tileSize <= 64 && 'h-4 w-4', tileSize <= 32 && 'h-2 w-2')} />
       </div>
     </div>
   );
